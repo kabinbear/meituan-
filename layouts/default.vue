7ecf3ea20a04f800731274380619562b83fbfa60
kabinbear <kabinbear@gmail.com>
2019-06-30 22:07:18 +0800

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-container class="layout-default">
+    <el-header height="197px"><MyHeader/></el-header>
+    <el-main>
+      <nuxt/>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+<script>
+  import MyHeader from  '@/components/public/header/index.vue'
+  export default {
+    components:{
+      MyHeader  
+    }
+  }
+</script>
+<style>
+</style>
